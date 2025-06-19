5940dfc7eaa0acda29341f79453c2cdf55344ddf
johurul-islam-cse <islam241-15-629@diu.edu.bd>
2025-06-19 21:32:15 +0600

    Add Sprin boot helper

---
+++ src/main/java/com/agromart/agromart/AgromartApplication.java
@@ -0,0 +1,13 @@
+package com.agromart.agromart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgromartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgromartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agromart/agromart/AgromartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agromart.agromart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgromartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
